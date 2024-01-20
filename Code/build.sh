12100429c329c267651e1a59e075b08b8eb9bf9c
TomasKopunec <tomas.kopunec5@gmail.com>
2024-01-20 16:18:37 +0000

    Improved build script

--- Code/build.sh
+++ Code/build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+echo "[Fetching Submodules]"
+git submodule update --init --recursive
+
+echo "[Building IF]"
 cd if-unofficial-models
 npm cache clean --force
 npm install
 yarn build
 
+echo "[Building IF Models]"
 cd ../if
 npm cache clean --force
 npm install
 npm install "@grnsft/if-models" "@grnsft/if-unofficial-models"
 yarn build
+
+echo "Done."
