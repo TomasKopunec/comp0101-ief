5a0b31f28edb4afa192b0cde44f8776176226f06
TomasKopunec <tomas.kopunec5@gmail.com>
2024-01-21 15:39:01 +0000

    Replaced build with init script

--- Code/init.sh
+++ Code/init.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # This script is used to initialize the environment for the project.
-echo "Cloning submodules..."
+echo "[Cloning Submodules]"
 git submodule update --init --recursive
 git submodule update --recursive --remote
+echo "Done."
 
-echo "Installing dependencies..."
+echo "[Installing Dependencies]"
 npm install -g typescript
 cd if
 npm install
 npm install '@grnsft/if-models' '@grnsft/if-unofficial-models'
-cd ../if-unofficial-models && npm install && cd ..
+cd ../if-unofficial-models
+npm install
+
+echo "Done."
