45dbd81e9be9d3bc24fb1ec58e608928de2de304
mosalehio <mosaleh2499@gmail.com>
2024-01-29 19:31:04 +0000

    update model cache data and to check cloud vendor

--- Code/if-optimisation-models/src/lib/right-sizing/index.ts
+++ Code/if-optimisation-models/src/lib/right-sizing/index.ts
@@ -1,56 +1,67 @@
 import { ModelPluginInterface } from '@grnsft/if-models/build/interfaces';
 import { ModelParams } from '@grnsft/if-models/build/types/common';
-// const { loadCPUData } = require('./testAzureCPUFamily');
 import { CPUDatabase, CloudInstance } from './azureCPUFamily';
 
 export class RightSizingModel implements ModelPluginInterface {
 
     private database: CPUDatabase;
+    private Cache: Map<string, CPUDatabase>;
 
     constructor(database: CPUDatabase = new CPUDatabase()) {
         this.database = database;
+        this.Cache = new Map<string, CPUDatabase>();
     }
 
     public async configure(configParams: object | undefined): Promise<ModelPluginInterface> {
-        await this.database.loadModelData('./data/azure-instances.json');
-        if (configParams) {
-            if ('data-path' in configParams) {
-                // The path to a JSON file containing the instance data is being passed in.
-                const instanceDataPath = configParams['data-path'];
-                if (instanceDataPath) {
-                    if (typeof instanceDataPath === 'string') {
-                        await this.database.loadModelData(instanceDataPath);
-                    } else {
-                        console.error('Error: Invalid instance data path type.');
-                    }
-                }
+        if (configParams && 'data-path' in configParams) {
+            const instanceDataPath = configParams['data-path'];
+            if (typeof instanceDataPath === 'string') {
+                await this.database.loadModelData(instanceDataPath);
+            } else {
+                console.error('Error: Invalid instance data path type.');
             }
         }
         return this;
     }
-
-    /**
+    
+        /**
      * Calculate the total emissions for a list of inputs.
      */
-    public execute(inputs: ModelParams[]): Promise<ModelParams[]> {
-        return Promise.resolve(inputs.map<ModelParams>((input) => {
-            if ('cloud-instance-type' in input && 'cpu-util' in input) {
-                input['old-instance'] = input['cloud-instance-type'];
-                input['old-cpu-util'] = input['cpu-util'];
-                let instance = this.database.getInstancesByModel(input['cloud-instance-type']);
-                let util = input['cpu-util'];
-                let res = this.calculateRightSizing(instance, util);
-                instance = res[0];
-                util = res[1];
-                input['cloud-instance-type'] = instance.model;
-                input['cpu-util'] = util;
+    public async execute(inputs: ModelParams[]): Promise<ModelParams[]> {
+        for (const input of inputs) {
+            if ('cloud-vendor' in input) {
+                const cloudVendor = input['cloud-vendor'];
+                if (!this.Cache.has(cloudVendor)) {
+                    const newDatabase = new CPUDatabase();
+                    if (cloudVendor === 'aws') {
+                        await newDatabase.loadModelData('./data/aws-instances.json');
+                    } else if (cloudVendor === 'azure') {
+                        await newDatabase.loadModelData('./data/azure-instances.json');
+                    }
+                    this.Cache.set(cloudVendor, newDatabase);
+                }
+                this.database = this.Cache.get(cloudVendor)!;
             }
-            return input;
-        }));
+            this.processInput(input);
+        }
+        return Promise.resolve(inputs);
+    }
+
+    private processInput(input: ModelParams): void {
+        if ('cloud-instance-type' in input && 'cpu-util' in input) {
+            input['old-instance'] = input['cloud-instance-type'];
+            input['old-cpu-util'] = input['cpu-util'];
+            let instance = this.database.getInstancesByModel(input['cloud-instance-type']);
+            let util = input['cpu-util'];
+            let res = this.calculateRightSizing(instance, util);
+            instance = res[0];
+            util = res[1];
+            input['cloud-instance-type'] = instance.model;
+            input['cpu-util'] = util;
+        }
     }
 
     private calculateRightSizing(cloudInstance: CloudInstance | null, cpuUtil: number): [CloudInstance, number] {
-
         if (cpuUtil < 0 || cpuUtil > 1) {
             throw new Error('CPU utilization must be between 0 and 1');
         }
