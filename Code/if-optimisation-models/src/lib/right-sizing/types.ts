d2073b0e4d84a06ace0038de503d8ed457df5aad
Fan <ucabj52@ucl.ac.uk>
2024-01-29 12:23:10 +0000

    Add types group and small update in README

---
+++ Code/if-optimisation-models/src/lib/right-sizing/types.ts
@@ -0,0 +1,9 @@
+export type InstanceInput = {
+    'cpu-cores-utilized': string;
+    'cpu-cores-available': string;
+    'memory-available': string;
+    'cpu-model-name': string;
+    'cpu-tdp': string;
+    'copu-instance-type':string;
+    'cpu-util':number;
+  };
