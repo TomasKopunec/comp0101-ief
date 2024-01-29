bc18b2887c4eae8d7aa13cbe2a5c0f96a64b335b
wangyiyuelva <yiyuwang891120@gmail.com>
2024-01-29 17:16:15 +0000

    Fix cpu-util result

--- Code/if-optimisation-models/src/lib/right-sizing/index.ts
+++ Code/if-optimisation-models/src/lib/right-sizing/index.ts
@@ -7,17 +7,17 @@ export class RightSizingModel implements ModelPluginInterface {
 
     private database: CPUDatabase;
 
-    constructor(database: CPUDatabase = new CPUDatabase()){
+    constructor(database: CPUDatabase = new CPUDatabase()) {
         this.database = database;
     }
 
     public async configure(configParams: object | undefined): Promise<ModelPluginInterface> {
         await this.database.loadModelData('./data/azure-instances.json');
-        if (configParams){
+        if (configParams) {
             if ('data-path' in configParams) {
                 // The path to a JSON file containing the instance data is being passed in.
                 const instanceDataPath = configParams['data-path'];
-                if (instanceDataPath){
+                if (instanceDataPath) {
                     if (typeof instanceDataPath === 'string') {
                         this.database.loadModelData(instanceDataPath);
                     } else {
@@ -32,7 +32,7 @@ export class RightSizingModel implements ModelPluginInterface {
     /**
      * Calculate the total emissions for a list of inputs.
      */
-    public execute(inputs: ModelParams[]): Promise<ModelParams[]>{
+    public execute(inputs: ModelParams[]): Promise<ModelParams[]> {
         return Promise.resolve(inputs.map<ModelParams>((input) => {
             if ('cloud-instance-type' in input && 'cpu-util' in input) {
                 input['old-instance'] = input['cloud-instance-type'];
@@ -46,31 +46,31 @@ export class RightSizingModel implements ModelPluginInterface {
                 input['cpu-util'] = util;
             }
             return input;
-        }));   
+        }));
     }
 
-    private calculateRightSizing(cloudInstance: CloudInstance | null, cpuUtil: number) : [CloudInstance, number]{
+    private calculateRightSizing(cloudInstance: CloudInstance | null, cpuUtil: number): [CloudInstance, number] {
 
-        if (cpuUtil < 0 || cpuUtil > 1){
+        if (cpuUtil < 0 || cpuUtil > 1) {
             throw new Error('CPU utilization must be between 0 and 1');
         }
 
-        if (!cloudInstance){
+        if (!cloudInstance) {
             throw new Error('Cloud instance not found');
         }
-        
+
         let family = this.database.getModelFamily(cloudInstance.model);
         let requireCPU = Math.ceil(cloudInstance.vCPUs * cpuUtil);
         let instance = cloudInstance;
-        if (family instanceof Array){
+        if (family instanceof Array) {
             family.forEach((cpu) => {
-                if (cpu.vCPUs >= requireCPU){
+                if (cpu.vCPUs >= requireCPU) {
                     if (cpu.vCPUs < instance.vCPUs) {
                         instance = cpu;
                     }
                 }
             });
         }
-        return [instance, instance.vCPUs / requireCPU];
+        return [instance, requireCPU / instance.vCPUs];
     }
 }
