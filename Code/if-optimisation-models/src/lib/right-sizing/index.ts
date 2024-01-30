9e370dc59654fbc1813dba37b6df17c62933c643
Derek Wang <derek_biobean@outlook.com>
2024-01-30 21:10:57 +0000

    Code cleanup & integrate type checking from Fan's legacy code

--- Code/if-optimisation-models/src/lib/right-sizing/index.ts
+++ Code/if-optimisation-models/src/lib/right-sizing/index.ts
@@ -1,11 +1,17 @@
+import { z, object } from 'zod';
+
 import { ModelPluginInterface } from '@grnsft/if-models/build/interfaces';
 import { ModelParams } from '@grnsft/if-models/build/types/common';
-import { CPUDatabase, CloudInstance } from './azureCPUFamily';
+
+import { CPUDatabase, CloudInstance } from './CPUFamily';
+import { validate, atLeastOneDefined } from '../../util/validations';
 
 export class RightSizingModel implements ModelPluginInterface {
 
     private database: CPUDatabase;
     private Cache: Map<string, CPUDatabase>;
+
+    private cpuMetrics = ['cloud-instance-type', 'cloud-vendor', 'cpu-util'];
 
     constructor(database: CPUDatabase = new CPUDatabase()) {
         this.database = database;
@@ -24,76 +30,91 @@ export class RightSizingModel implements ModelPluginInterface {
         return this;
     }
 
-        /**
+    /**
      * Calculate the total emissions for a list of inputs.
      */
-        public async execute(inputs: ModelParams[]): Promise<ModelParams[]> {
-            let outputs: ModelParams[] = [];
-        
-            for (const input of inputs) {
-                if ('cloud-vendor' in input) {
-                    const cloudVendor = input['cloud-vendor'];
-                    if (!this.Cache.has(cloudVendor)) {
-                        const newDatabase = new CPUDatabase();
-                        if (cloudVendor === 'aws') {
-                            await newDatabase.loadModelData('./data/aws-instances.json');
-                        } else if (cloudVendor === 'azure') {
-                            await newDatabase.loadModelData('./data/azure-instances.json');
-                        }
-                        this.Cache.set(cloudVendor, newDatabase);
+    public async execute(inputs: ModelParams[]): Promise<ModelParams[]> {
+        let outputs: ModelParams[] = [];
+
+        for (const input of inputs) {
+            if ('cloud-vendor' in input) {
+                const cloudVendor = input['cloud-vendor'];
+                if (!this.Cache.has(cloudVendor)) {
+                    const newDatabase = new CPUDatabase();
+                    if (cloudVendor === 'aws') {
+                        await newDatabase.loadModelData('./data/aws-instances.json');
+                    } else if (cloudVendor === 'azure') {
+                        await newDatabase.loadModelData('./data/azure-instances.json');
                     }
-                    this.database = this.Cache.get(cloudVendor)!;
+                    this.Cache.set(cloudVendor, newDatabase);
                 }
-        
-                let processedOutputs = this.processInput(input);
-                outputs.push(...processedOutputs);
+                this.database = this.Cache.get(cloudVendor)!;
             }
-            return Promise.resolve(outputs);
+
+            let processedOutputs = this.processInput(input);
+            outputs.push(...processedOutputs);
         }
-        
-        private processInput(input: ModelParams): ModelParams[] {
-            let outputs: ModelParams[] = [];
-            if ('cloud-instance-type' in input && 'cpu-util' in input) {
-                input['old-instance'] = input['cloud-instance-type'];
-                input['old-cpu-util'] = input['cpu-util'];
-        
-                let instance = this.database.getInstancesByModel(input['cloud-instance-type']);
-                let util = parseFloat(input['cpu-util']); // ensure cpu-util is a number
-                let res = this.calculateRightSizing(instance, util);
-        
-                // for each instance combination, create a new output
-                res.forEach(([instance, util]) => {
-                    let output = { ...input }; // copy input to create new output
-                    output['cloud-instance-type'] = instance.model;
-                    output['cpu-util'] = util.toString(); // convert back to string if needed
-                    outputs.push(output);
-                });
-            } else {
-                outputs.push(input); // push input unchanged if not processing
-            }
-            return outputs;
+        return Promise.resolve(outputs);
+    }
+
+    private validateSingleInput(input: ModelParams) {
+        const schema = z
+            .object({
+                'cloud-instance-type': z.string(),
+                'cloud-vendor': z.string(),
+                'cpu-util': z.number().gte(0).lte(1)
+            })
+            .refine(atLeastOneDefined, {
+                message: `At least one of ${this.cpuMetrics} should present.`,
+            });
+
+        return validate<z.infer<typeof schema>>(schema, input);
+    }
+
+
+    private processInput(input: ModelParams): ModelParams[] {
+        let outputs: ModelParams[] = [];
+        if (this.validateSingleInput(input)) {
+            input['old-instance'] = input['cloud-instance-type'];
+            input['old-cpu-util'] = input['cpu-util'];
+
+            let instance = this.database.getInstancesByModel(input['cloud-instance-type']);
+            let util = parseFloat(input['cpu-util']); // ensure cpu-util is a number
+            let res = this.calculateRightSizing(instance, util);
+
+            // for each instance combination, create a new output
+            res.forEach(([instance, util]) => {
+                let output = { ...input }; // copy input to create new output
+                output['cloud-instance-type'] = instance.model;
+                output['cpu-util'] = util.toString(); // convert back to string if needed
+                outputs.push(output);
+            });
+        } else {
+            outputs.push(input); // push input unchanged if not processing
         }
+        return outputs;
+    }
 
     private calculateRightSizing(cloudInstance: CloudInstance | null, cpuUtil: number): [CloudInstance, number][] {
         if (cpuUtil < 0 || cpuUtil > 1) {
             throw new Error('CPU utilization must be between 0 and 1');
         }
-    
+
         if (!cloudInstance) {
             throw new Error('Cloud instance not found');
         }
-    
+
         let family = this.database.getModelFamily(cloudInstance.model);
         if (!(family instanceof Array)) {
             return [[cloudInstance, cpuUtil]];
         }
-    
+
         // calculate the required vCPUs based on the requested CPU utilization
         let requiredCPUs = Math.ceil(cloudInstance.vCPUs * cpuUtil);
         let sortedFamily = family.sort((a, b) => b.vCPUs - a.vCPUs); // Sort instances by vCPUs descending
         let optimalCombination: [CloudInstance, number][] = [];
         let remainingCPUs = requiredCPUs;
-    
+
         // iterate over the sorted family and select instances to fulfill the required vCPUs
         for (const instance of sortedFamily) {
             while (remainingCPUs - instance.vCPUs >= 0) {
@@ -102,7 +123,7 @@ export class RightSizingModel implements ModelPluginInterface {
             }
             if (remainingCPUs === 0) break; // stop if we've matched the required vCPUs
         }
-    
+
         // if there's a shortfall, use the next smallest instance to cover the remaining CPUs
         if (remainingCPUs > 0) {
             const nextSmallestInstance = sortedFamily.find(inst => inst.vCPUs >= remainingCPUs);
--- Code/if-optimisation-models/src/lib/right-sizing/types.ts
+++
@@ -1,9 +0,0 @@
-export type InstanceInput = {
-    'cpu-cores-utilized': string;
-    'cpu-cores-available': string;
-    'memory-available': string;
-    'cpu-model-name': string;
-    'cpu-tdp': string;
-    'copu-instance-type':string;
-    'cpu-util':number;
-  };
