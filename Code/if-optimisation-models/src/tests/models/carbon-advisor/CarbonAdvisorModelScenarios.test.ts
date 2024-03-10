41e94ddc2b357b897e60489e5ac22f0e914a276f
Tomas Kopunec <tomas.kopunec5@gmail.com>
2024-03-10 19:00:26 +0000

    Sampling scenarios added

--- Code/if-optimisation-models/src/tests/models/carbon-advisor/CarbonAdvisorModelScenarios.test.ts
+++ Code/if-optimisation-models/src/tests/models/carbon-advisor/CarbonAdvisorModelScenarios.test.ts
@@ -20,6 +20,9 @@ import scenario4 = require("./scenarios/scenario4.json");
 import scenario5 = require("./scenarios/scenario5.json");
 import scenario6 = require("./scenarios/scenario6.json");
 import scenario7 = require("./scenarios/scenario7.json");
+import scenario8 = require("./scenarios/scenario8.json");
+import scenario9 = require("./scenarios/scenario9.json");
+import scenario10 = require("./scenarios/scenario10.json");
 
 import locations = require("../../../lib/carbon-aware-advisor/locations.json");
 
@@ -198,11 +201,11 @@ describe('CarbonAdvisorModel.Basic', () => {
         expect(suggestion.rating).toBe(15);
     });
 
-     /** 
-     * Scenario 7: Find best time for 15/01/2024 in world_azure locations (big dataset)
-     * Expected: Suggest the time of 14:00 with score 19
-     */
-     it('CarbonAdvisorModel.Scenario7', async () => {
+    /** 
+    * Scenario 7: Find best time for 15/01/2024 in world_azure locations (big dataset)
+    * Expected: Suggest the time of 14:00 with score 19
+    */
+    it('CarbonAdvisorModel.Scenario7', async () => {
         initMock(scenario7);
         const config: ConfigParams = {
             "allowed-locations": ["world_azure"],
@@ -226,6 +229,189 @@ describe('CarbonAdvisorModel.Basic', () => {
     });
 });
 
+describe('CarbonAdvisorModel.Sampling', () => {
+    /**
+     * Validate README assumption:
+     * A sampling number of samples for trade-off visualization. 
+     * A best combination from each timeframe is always included.
+     * So sampling **must be >= number of time regions in the allowed-timeframes***. 
+     */
+    it('CarbonAdvisorModel.Sampling.Scenario0', async () => {
+        initMock(scenario1);
+        const config: ConfigParams = {
+            "allowed-locations": ["eastus"],
+            "allowed-timeframes": ["2024-01-15T00:00:00Z - 2024-01-16T00:00:00Z"],
+            "sampling": 2
+        };
+        const inputs: PluginParams[] = [{
+            "timestamp": "",
+            "duration": 1
+        }];
+        const model = CarbonAwareAdvisor(config);
+        await expect(model.execute(inputs))
+            .rejects.toThrow("Sampling must be greater than or equal to allowed timeframes");
+    });
+
+    it('CarbonAdvisorModel.Sampling.Scenario1', async () => {
+        initMock(scenario1);
+        const config: ConfigParams = {
+            "allowed-locations": ["eastus"],
+            "allowed-timeframes": ["2024-01-15T12:00:00Z - 2024-01-15T18:00:00Z"],
+            "sampling": 1
+        };
+        const inputs: PluginParams[] = [{
+            "timestamp": "",
+            "duration": 1
+        }];
+        const model = CarbonAwareAdvisor(config);
+        mockAverage(model, 5);
+        const result = await model.execute(inputs);
+        validateSampling(result, config);
+    });
+
+    it('CarbonAdvisorModel.Sampling.Scenario2', async () => {
+        initMock(scenario8);
+        const config: ConfigParams = {
+            "allowed-locations": ["eastus"],
+            "allowed-timeframes": ["2024-01-14T00:00:00Z - 2024-01-17T00:00:00Z"],
+            "sampling": 3
+        };
+        const inputs: PluginParams[] = [{
+            "timestamp": "",
+            "duration": 1
+        }];
+        const model = CarbonAwareAdvisor(config);
+        mockAverage(model, 5);
+        const result = await model.execute(inputs);
+        validateSampling(result, config);
+        console.log(JSON.stringify(result, null, 2));
+    });
+
+    it('CarbonAdvisorModel.Sampling.Scenario3', async () => {
+        initMock(scenario9);
+        const config: ConfigParams = {
+            "allowed-locations": ["eastus", "westus", "centralus"],
+            "allowed-timeframes": ["2024-01-01T00:00:00Z - 2024-01-22T00:00:00Z"],
+            "sampling": 10
+        };
+        const inputs: PluginParams[] = [{
+            "timestamp": "",
+            "duration": 1
+        }];
+        const model = CarbonAwareAdvisor(config);
+        const result = await model.execute(inputs);
+        const suggestions = result[0].suggestions as Suggestion[];
+
+        // Check if all suggestions have the same score
+        expect(suggestions.length).toBe(4);
+        for (const suggestion of suggestions) {
+            // Location in allowed-locations
+            expect(config["allowed-locations"].includes(suggestion.location)).toBe(true);
+            expect(suggestion.rating).toBe(1);
+        }
+
+        validateSampling(result, config);
+    });
+
+    it('CarbonAdvisorModel.Sampling.Scenario4', async () => {
+        const data = scenario8;
+        data.sort((a, b) => a.rating - b.rating);
+
+        initMock(data);
+        const config: ConfigParams = {
+            "allowed-locations": ["eastus"],
+            "allowed-timeframes": ["2024-01-14T00:00:00Z - 2024-01-17T00:00:00Z"],
+            "sampling": 5
+        };
+        const inputs: PluginParams[] = [{
+            "timestamp": "",
+            "duration": 1
+        }];
+        const model = CarbonAwareAdvisor(config);
+        const result = await model.execute(inputs);
+        validateSampling(result, config);
+
+        // Sort plotted points
+        const plotted_points = (result[0].plotted_points as Suggestion[]).sort((a, b) => a.rating - b.rating);
+        expect(plotted_points[0]).toEqual(data[0]);
+        expect(plotted_points[1]).toEqual(data[1]);
+
+        let lastRating = plotted_points[1].rating;
+        for (let i = 2; i < 5; i++) {
+            expect(plotted_points[i].rating).toBeGreaterThan(lastRating);
+            expect(config["allowed-locations"].includes(plotted_points[i].location)).toBe(true);
+        }
+    });
+
+    /**
+     * Validate the following:
+     * If you have 2 timeframes and 5 samples then the samples will be:
+     * The best for timeframe one
+     * The best for timefraume 2
+     * 2 at random from the largest timeframe
+     * 1 at random from the smallest timeframe
+     */
+    it('CarbonAdvisorModel.Sampling.Scenario5', async () => {
+        initMock(scenario10);
+        const config: ConfigParams = {
+            "allowed-locations": ["eastus"],
+            "allowed-timeframes": [
+                "2024-01-01T00:00:00Z - 2024-01-02T00:00:00Z",
+                "2024-01-03T04:00:00Z - 2024-01-03T20:00:00Z"
+            ],
+            "sampling": 5
+        };
+        const inputs: PluginParams[] = [{
+            "timestamp": "",
+            "duration": 1
+        }];
+        const model = CarbonAwareAdvisor(config);
+        const result = await model.execute(inputs);
+        validateSampling(result, config);
+
+        const plotted_points = (result[0].plotted_points as Suggestion[]).sort((a, b) => a.rating - b.rating);
+        const sortedInput = scenario10.sort((a, b) => a.rating - b.rating);
+
+        console.log("Plotted", JSON.stringify(plotted_points, null, 2));
+
+        // The best for timeframe one
+        const fst = plotted_points[0];
+        expect(fst.location).toBe("eastus");
+        expect(fst.time).toBe("2024-01-01T10:00:00+00:00");
+        expect(fst.rating).toBe(6);
+
+        // The best for timeframe two
+        const snd = plotted_points[1];
+        expect(snd.location).toBe("eastus");
+        expect(snd.time).toBe("2024-01-03T02:00:00+00:00");
+        expect(snd.rating).toBe(4);
+
+        const lastRating = plotted_points[1].rating;
+        expect(plotted_points[2].time.startsWith("2024-01-01T")).toBe(true);
+        expect(plotted_points[2].rating).toBeGreaterThanOrEqual(lastRating);
+
+        expect(plotted_points[3].time.startsWith("2024-01-01T")).toBe(true);
+        expect(plotted_points[3].rating).toBeGreaterThanOrEqual(lastRating);
+
+        expect(plotted_points[4].time.startsWith("2024-01-03T")).toBe(true);
+        expect(plotted_points[4].rating).toBeGreaterThanOrEqual(lastRating);
+    });
+});
+
+function validateSampling(result: PluginParams[], config: ConfigParams) {
+    console.log("Validating sampling:");
+    const suggestions = result[0].suggestions as Suggestion[];
+    const plotted_points = result[0].plotted_points as Suggestion[];
+
+    // Check if the number of plotted points is equal to the sampling config
+    expect(plotted_points.length).toBe(config["sampling"]);
+
+    // Check if all plottings are within the allowed locations and timeframes
+    checkIfSuggestedLocationsWithinAllowedLocations(config, plotted_points);
+    checkIfSuggestedTimeframesWithinRange(config, plotted_points);
+    return suggestions;
+}
+
 function validateSuggestions(result: PluginParams[], config: ConfigParams): Suggestion[] {
     console.log("Validating suggestions:");
     console.log(JSON.stringify(result, null, 2));
@@ -241,8 +427,8 @@ function validateSuggestions(result: PluginParams[], config: ConfigParams): Sugg
 function checkIfSuggestedLocationsWithinAllowedLocations(config: ConfigParams, suggestions: Suggestion[]) {
     const allowedLocations = config["allowed-locations"] as string[];
     const map = locations as Record<string, string[]>;
-    for(const loc of allowedLocations) {
-        if(loc in locations) {
+    for (const loc of allowedLocations) {
+        if (loc in locations) {
             allowedLocations.push(...map[loc]);
         }
     }
@@ -274,7 +460,7 @@ function checkIfSuggestedTimeframesWithinRange(config: ConfigParams, suggestions
             expect(dateWithDuration, `Suggested duration must be a valid date`).not.toBe("Invalid Date");
 
             const isWithinBounds = (date >= from && date <= to) || (dateWithDuration >= from && dateWithDuration <= to);
-            console.log(`Checking if ${date} (or ${dateWithDuration}) is between ${from} and ${to}`)
+            // console.log(`Checking if ${date} (or ${dateWithDuration}) is between ${from} and ${to}`)
             isValid = isValid || isWithinBounds;
         }
         // Must be within the allowed timeframes
