1103c3662bea494913a17b51e2a1f3008b52d25e
jimbou <el17072@mail.ntua.gr>
2024-03-12 19:48:46 +0200

    completed testing for plotter

--- test_tool/plotter/all_test_plotter.sh
+++ test_tool/plotter/all_test_plotter.sh
@@ -2,6 +2,14 @@
 
 ./test_plotter.sh test_plotter1 test1.yml
 ./test_plotter.sh test_plotter2 test2.yml
-
-
- 
+./test_plotter.sh test_plotter3 test3.yml
+./test_plotter.sh test_plotter4 test4.yml
+./test_plotter.sh test_plotter5 test5.yml
+./test_plotter.sh test_plotter6 test6.yml
+./test_plotter.sh test_plotter7 test7.yml
+./test_plotter.sh test_plotter8 test8.yml
+./test_plotter.sh test_plotter9 test9.yml
+./test_plotter.sh test_plotter10 test10.yml
+./test_plotter.sh test_plotter11 test11.yml
+./test_plotter.sh test_plotter12 test12.yml
+./test_plotter.sh test_plotter12 test13.yml
