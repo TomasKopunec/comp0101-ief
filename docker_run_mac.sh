c9afbeb5de9d43566634cf9ae2ef508ca48f92e4
JasonLuuk <JasonLuuk@outlook.com>
2024-03-23 00:21:23 +0000

    Update docker_run_mac.sh
    Co-Authored-By: wangyiyuelva <56147414+wangyiyuelva@users.noreply.github.com>

--- docker_run_mac.sh
+++ docker_run_mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build --no-cache  -t ief_image -f Dockerfile.mac .
-echo "Docker image built."
+echo "Docker Mac image built."
 
 docker run -it --net=host --name ief_runner ief_image
